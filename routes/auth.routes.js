ce307600dea956bb770441fa01aa6f7ebbb10730
patricio <patoare@hotmail.com>
2025-01-20 16:19:29 +0100

    first postman checks

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const bcrypt = require('bcryptjs')
 const User = require('../models/User.moodel')
 const jwt = require('jsonwebtoken')
+const { isAuthenticated } = require('../middlewares/route-guard.middleware')
 
 router.get('/', (req, res) => {
   res.json('All good in auth')
@@ -15,7 +16,11 @@ const salt = bcrypt.genSaltSync(13)
 const passwordHash = bcrypt.hashSync(credentials.password, salt)
 
 try {
-const newUser = await User.create(...credentials, passwordHash)
+const newUser = await User.create({
+  username: credentials.username,
+  email: credentials.email,
+  passwordHash: passwordHash,
+})
 res.status(201).json()
 } catch(error) {
   next(error)
@@ -29,7 +34,7 @@ const potentialUser = await User.findOne({username: credentials.username})
 if(potentialUser) {
   if(bcrypt.compareSync(credentials.password, potentialUser.passwordHash)) {
     const payload = {userId: potentialUser._id}
-  const authToken = jwt.sign(payload, process.env.TOKEN_SECRET,
+    const authToken = jwt.sign(payload, process.env.TOKEN_SECRET,
     { algorithm: 'HS256',
       expiresIn: "6h"}
   )
@@ -45,5 +50,14 @@ res.json(authToken)
 }
 })
 //Get Verify
+router.get('/verify', isAuthenticated, async(req, res, next) => {
+  try {
+    const currentUser = await User.findById(req.tokenPayload.userId)
+  } catch(error) {
+    next(error)
+  }
+ res.json('estas viendo este rdo porque el tokes es correcto')
+})
+
 
 module.exports = router
