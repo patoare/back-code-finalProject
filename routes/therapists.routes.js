c06ad1a2e72e65040810773aa10f08c622cd0922
patricio <patoare@hotmail.com>
2025-01-23 12:08:39 +0100

    display all therap

--- routes/therapists.routes.js
+++ routes/therapists.routes.js
@@ -1,7 +1,15 @@
+const { isAuthenticated } = require('../middlewares/route-guard.middleware')
+const User = require('../models/User.moodel')
+const { isValidObjectId } = require('mongoose')
 const router = require('express').Router()
 
-router.get('/', (req, res) => {
-  res.json('All good in here')
+router.get('/', async(req, res, next) => {
+  try{
+    const user = await User.find()
+    res.json(user)
+  }catch(error) {
+    next(error)
+  }
 })
 
 module.exports = router
